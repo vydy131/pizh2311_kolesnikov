92207b76a402e79547d2c7021ec993f62c39ba62
Vlad Kolesnikov <vkolesnikov@cloudfactory.com>
2025-04-10 15:25:00 +0300

    LR CPP 2 WIP

--- labwork2/lib/number.cpp
+++ labwork2/lib/number.cpp
@@ -1,40 +1,84 @@
 #include "number.h"
-
+#include <cstring>
+#include <stdexcept>
 
 uint2022_t from_uint(uint32_t i) {
-    return uint2022_t();
+    uint2022_t result;
+    for (int bit = 0; bit < 32; ++bit) {
+        result.set_bit(bit, (i >> bit) & 1);
+    }
+    return result;
 }
 
 uint2022_t from_string(const char* buff) {
-    return uint2022_t();
+    uint2022_t result;
+    size_t len = std::strlen(buff);
+    size_t index = 0;
+    for (size_t i = len; i > 0; --i) {
+        if (buff[i-1] == '1') {
+            result.set_bit(index, true);
+        }
+        ++index;
+    }
+    return result;
 }
 
 uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    uint2022_t result;
+    uint8_t carry = 0;
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        uint8_t sum = lhs[i] + rhs[i] + carry;
+        result[i] = sum;
+        carry = (sum < lhs[i]) ? 1 : 0;
+    }
+    return result;
 }
 
 uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    uint2022_t result;
+    uint8_t borrow = 0;
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        uint8_t diff = lhs[i] - rhs[i] - borrow;
+        result[i] = diff;
+        borrow = (diff > lhs[i]) ? 1 : 0;
+    }
+    return result;
 }
 
 uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    uint2022_t result;
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        for (size_t j = 0; j < rhs.size(); ++j) {
+            uint8_t product = lhs[i] * rhs[j];
+            result[i+j] += product;
+        }
+    }
+    return result;
 }
 
 uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return uint2022_t();
+    if (rhs == uint2022_t()) {
+        throw std::invalid_argument("Division by zero");
+    }
+    uint2022_t result;
+    uint2022_t remainder;
+    for (size_t i = lhs.size(); i-- > 0;) {
+        remainder.data.insert(remainder.data.begin(), lhs[i]);
+    }
+    return result;
 }
 
 bool operator==(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return false;
+    return lhs.data == rhs.data;
 }
 
 bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs) {
-    return false;
+    return !(lhs == rhs);
 }
 
 std::ostream& operator<<(std::ostream& stream, const uint2022_t& value) {
-    stream << "Implement me";
-
+    for (size_t i = 0; i < value.size(); ++i) {
+        stream << (int)value[i];
+    }
     return stream;
 }
--- labwork2/lib/number.h
+++ labwork2/lib/number.h
@@ -1,28 +1,53 @@
 #pragma once
 #include <cinttypes>
 #include <iostream>
-
+#include <vector>
 
 struct uint2022_t {
-    // implement
+    std::vector<uint8_t> data;
+
+    uint2022_t() : data(37, 0) {}
+
+    void set_bit(uint32_t index, bool value) {
+        size_t byte_index = index / 8;
+        size_t bit_index = index % 8;
+        if (value) {
+            data[byte_index] |= (1 << bit_index);
+        } else {
+            data[byte_index] &= ~(1 << bit_index);
+        }
+    }
+
+    bool get_bit(uint32_t index) const {
+        size_t byte_index = index / 8;
+        size_t bit_index = index % 8;
+        return data[byte_index] & (1 << bit_index);
+    }
+
+    size_t size() const {
+        return data.size();
+    }
+
+    uint8_t& operator[](size_t index) {
+        return data[index];
+    }
+
+    const uint8_t& operator[](size_t index) const {
+        return data[index];
+    }
 };
 
-static_assert(sizeof(uint2022_t) <= 300, "Size of uint2022_t must be no higher than 300 bytes");
-
+// Функции конвертации
 uint2022_t from_uint(uint32_t i);
-
 uint2022_t from_string(const char* buff);
 
+// Операторы
 uint2022_t operator+(const uint2022_t& lhs, const uint2022_t& rhs);
-
 uint2022_t operator-(const uint2022_t& lhs, const uint2022_t& rhs);
-
 uint2022_t operator*(const uint2022_t& lhs, const uint2022_t& rhs);
-
 uint2022_t operator/(const uint2022_t& lhs, const uint2022_t& rhs);
 
 bool operator==(const uint2022_t& lhs, const uint2022_t& rhs);
-
 bool operator!=(const uint2022_t& lhs, const uint2022_t& rhs);
 
 std::ostream& operator<<(std::ostream& stream, const uint2022_t& value);
